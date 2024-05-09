a11c089aa04dc6a82baae225890aadf6c0a752ab
liyixin123 <592945148@qq.com>
2024-05-09 14:33:29 +0800

    修改界面部分，准备兼容 file_hasher 的功能

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod upload;
 
-use iced::widget::{column, container, text, Button, Text};
-use iced::{
-    event, executor, Alignment, Application, Command, Element, Font, Length, Settings,
-    Subscription, Theme,
-};
+use std::fmt::{Display, Formatter};
+use iced::widget::{column, container, Button, pick_list, row};
+use iced::{event, executor, Alignment, Application, Command, Element, Font, Length, Settings, Subscription, Theme, widget};
 use rfd::FileDialog;
 use std::option::Option;
 use std::path::PathBuf;
+use iced::widget::text::Shaping::Advanced;
+
 
 pub fn main() -> iced::Result {
     let setting = Settings {
@@ -25,12 +25,46 @@ enum Message {
     DraggedImage(iced::Event),
     ImageDropped(Option<PathBuf>),
     OpenImgPressed,
+    OnSelectOption(BaseOption),
+
     OtherEvent(),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum BaseOption {
+    #[default]
+    PicUpload,
+    FileMD5,
+}
+
+impl Display for BaseOption {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                BaseOption::PicUpload => { "PicUpload" }
+                BaseOption::FileMD5 => { "FileMD5" }
+            }
+        )
+    }
+}
+
+impl BaseOption {
+    const ALL: [BaseOption; 2] = [BaseOption::PicUpload, BaseOption::FileMD5];
+    fn hint(&self) -> &str {
+        match self {
+            BaseOption::PicUpload => { "上传图片到github，将Markdown链接写入剪贴板" }
+            BaseOption::FileMD5 => { "计算文件 md5，并生成文件，同时写入剪贴板" }
+        }
+    }
 }
 
 struct MyApp {
     return_path: Option<String>,
     image_path: Option<PathBuf>,
+    selected_option: Option<BaseOption>,
+    option_hint: String,
 }
 
 impl Application for MyApp {
@@ -44,6 +78,8 @@ impl Application for MyApp {
             MyApp {
                 return_path: None,
                 image_path: None,
+                selected_option: None,
+                option_hint: "".to_string(),
             },
             Command::none(),
         )
@@ -63,7 +99,6 @@ impl Application for MyApp {
                         Err(err) => self.return_path = Some(err.to_string()),
                     }
                 }
-
                 Command::none()
             }
             Message::DraggedImage(event) => {
@@ -81,31 +116,83 @@ impl Application for MyApp {
                 Message::ImageDropped,
             ),
             Message::OtherEvent() => Command::none(),
+            Message::OnSelectOption(option) => {
+                self.selected_option = Some(option);
+                self.option_hint = option.hint().to_string();
+                Command::none()
+            }
         }
     }
     fn view(&self) -> Element<Message> {
-        let _label: Text = if let Some(path) = &self.return_path {
-            text(path)
+        let _label = if let Some(path) = &self.return_path {
+            widget::Text::from(path.as_str()).shaping(Advanced)
         } else {
-            text("Wait for drop file!")
+            widget::Text::from("等待文件拖放！").shaping(Advanced)
         };
+
         let _path_text = if let Some(path) = &self.image_path {
-            text(format!("Dropped Image Path: {:?}", path.display()))
+            iced::widget::text(format!("Dropped Image Path: {:?}", path.display()))
         } else {
-            text("Drop an image here")
+            let mut content: String = "选择或拖入待上传的图片".to_string();
+            match self.selected_option {
+                None => {}
+                Some(option) => {
+                    match option {
+                        BaseOption::PicUpload => {}
+                        BaseOption::FileMD5 => { content = "选择或拖入需要生成MD5的文件".to_string() }
+                    }
+                }
+            }
+            iced::widget::Text::new(content).shaping(Advanced)
         };
         let btn_open_image = Button::new(
-            text("Upload Image")
+            iced::widget::text("Upload Image")
                 .horizontal_alignment(iced::alignment::Horizontal::Center)
                 .vertical_alignment(iced::alignment::Vertical::Center)
                 .size(15),
         )
-        .on_press(Message::OpenImgPressed);
+            .on_press(Message::OpenImgPressed);
 
-        let content = column![btn_open_image, _label, _path_text,]
+        // let combo_box = combo_box(
+        //     &self.options,
+        //     "选择一个功能",
+        //     self.selected_option.as_ref(),
+        //     Message::OnSelectOption,
+        // )
+        //     .width(250)
+        //     .on_option_hovered(Message::OptionHovered);
+
+        let pick_list = pick_list(
+            &BaseOption::ALL[..],
+            self.selected_option,
+            Message::OnSelectOption,
+        )
+            .placeholder("选择功能")
+            .text_shaping(Advanced)
+            .width(200);
+
+        let option_hint = iced::widget::Text::new(self.option_hint.clone())
+            .horizontal_alignment(iced::alignment::Horizontal::Center)
+            .vertical_alignment(iced::alignment::Vertical::Center)
+            .shaping(Advanced);
+        let row = row![
+            iced::widget::text("功能选项：").shaping(Advanced),
+            pick_list,
+            option_hint,
+        ]
+            .spacing(20)
+            .width(Length::Fill)
+            ;
+
+        let content = column![
+            _label,
+            btn_open_image,
+            _path_text,
+            row,
+        ]
             .width(Length::Fill)
             .align_items(Alignment::Center)
-            .spacing(10)
+            .spacing(20)
             .padding(20);
 
         container(content)
